d26c3869cc8f93c39d89f9355bcd37f0ecff1415
sugimoto_e <eikun1014@gmail.com>
2024-11-02 19:10:57 +0900

    next → vite

--- web/src/utils/transport.ts
+++
@@ -1,7 +0,0 @@
-import { createConnectTransport } from '@connectrpc/connect-node';
-const baseUrl = process.env.API_BASE_URL || 'http://api:8000';
-export const baseTransport = createConnectTransport({
-  baseUrl: baseUrl,
-  httpVersion: '2',
-  interceptors: [],
-});
